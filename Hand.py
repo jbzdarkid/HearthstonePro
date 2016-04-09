d8925b77a5a079cc53feb1db52e994b03eb1f7d6
jbzdarkid <jbzdarkid@gmail.com>
2016-04-09 13:30:19 -0400

    Stuff happened, I didn't commit
    It does basically work now, though.

--- Hand.py
+++ Hand.py
@@ -1,3 +1,6 @@
+# Displaying with tk:
+# Use Toplevel() in construction, and root.lift() as a backup
+
 '''Cards with special properties:
 Dragon in hand: Blackwing Corruptor, Blackwing Technician, Twilight Welp, Alexstrasza's Champion, Wyrmrest Agent, Twilight Guardian, Chillmaw, Rend Blackhand
 Spare Part:
@@ -5,52 +8,56 @@ Return to hand: Ancient Brewmaster, Anub'ar Ambusher, Dream, Freezing Trap, Shad
 Other: Cursed!, Golden Monkey, King's Elekk, Gnomish Experimenter, Holy Wrath, Varian Wrynn, Shadowfiend, Chromaggus, Flame Leviathan, Emperor Thaurissan, Headcrack, Unstable Portal, Wild Growth, Mulch
 '''
 
-hand = []
-toDraw = []
-
-class Card():
-	def __init__(self, position=0, name=None, turn=0):
-		self.position = position
+class card():
+	def __init__(self, name, mulligan=False):
+		global turn
 		self.name = name
-		if self.turn == -1:
-			self.turn = 0
-			self.mulliganed = True
-		else:
-			self.turn = turn
-		self.notes = ''
+		self.turn = turn/2
+		self.mulligan = mulligan
 
-	def __eq__(self, other):
-		if type(other) is not type(self):
-			return self.name == other # Allows comparison between Card() and string, but only in that order!
-		elif self.name != other.name:
-			return False
-		elif self.position != other.position:
-			return False
-		elif self.turn != other.turn:
-			return False
-		return True
+	def __repr__(self):
+		print 'card(%s%s)' % (self.name, 'mulligan=True' if self.mulligan else '')
 
-	def __ne__(self, other):
-		return not self.__eq__(other)
+turn = 0
+hand = []
+names = [None]*64
+wentFirst = 0
 
-	def addNote(self, note):
-		self.notes += '\n'+note
+def reset():
+	global turn, hand, names, wentFirst
+	wentFirst = 0
+	turn = 0
+	hand = []
+	names = [None]*64
 
-def draw():
-	card = new Card()
-	if len(toDraw) > 0:
-		card.setNote('Drawn by'+toDraw.pop(0))
-	hand.append(new Card())
+reset()
 
-def processCardPlayed(event):
-	if event.kind != 'Card Played':
-		return
-	if event.source in ['Time Rewinder', 'Sap']:
-		toDraw += [event.target]
-	elif event.source == 'Emperor Thaurissan':
-		for card in hand:
-			card.addNote('Cost reduced by 1 by Emperor Thaurissan')
-	elif event.source == 'Sense Demons':
-		toDraw += [played.name]*2
-	elif event.source in ['Captain's Parrot', 'Call Pet']:
-		toDraw += [played.name]
+def draw(id):
+	global hand
+	hand.append(card(id))
+
+def play(pos):
+	global hand
+	card = hand.pop(pos)
+	print card
+
+	if card.name == 'Unstable Portal':
+		print '<56>'
+
+def mulligan(pos, id):
+	global hand
+	if hand[pos].name != id:
+		hand[pos] = card(id, mulligan=True)
+
+def turnover():
+	global turn, hand, wentFirst
+	turn += 1
+	if (turn+wentFirst)%2 == 0:
+		print 'Current Turn:', turn/2
+		print 'Card No. | Drawn on turn | Name | Other notes'
+		for i in range(len(hand)):
+			print '       %d |            %02d |  %s |' % (i, hand[i].turn, str(hand[i].name))
+
+def length():
+	global hand
+	return len(hand)
---
+++ Parser.py
@@ -0,0 +1,150 @@
+from ast import literal_eval
+from os import sep
+from os.path import expanduser
+from subprocess import Popen, PIPE
+import Hand
+
+try:
+	config = literal_eval(open('config.cfg', 'rb').read())
+except IOError:
+	# Config not defined, somehow. Recreate.
+	config = {}
+except SyntaxError:
+	# Config corrupt, somehow. Recreate.
+	config = {}
+
+if 'logconfig' not in config:
+	from platform import system
+	if system() == 'Windows':
+		config['logconfig'] = '%LocalAppData%'
+	elif system() == 'Darwin': # Mac OSX
+		config['logconfig'] = expanduser('~')+'/Library/Preferences'
+		name = 'Hearthstone.app'
+	else:
+		raise Exception('Unknown platform:', system())
+	config['logconfig'] += '/Blizzard/Hearthstone/log.config'
+
+if 'log' not in config:
+	from os import walk
+	for root, dirs, files in walk(expanduser('~')):
+		if name in dirs+files:
+			config['log'] = root + sep + 'Logs' + sep
+	if 'log' not in config:
+		raise Exception('Couldn\'t find Hearthstone install!')
+	print config['log']
+
+c = open('config.cfg', 'wb')
+c.write(str(config))
+c.close()
+
+f = open('log.config', 'rb').read()
+try:
+	g = open(config['logconfig'], 'rb').read()
+	if f != g:
+		# Config exists, but differs
+		g = open(config['logconfig'], 'wb')
+		g.write(f)
+		g.close()
+	# Config exists, same
+except IOError:
+	# Config doesn't exist
+	g = open(config['logconfig'], 'wb')
+	g.write(f)
+	g.close()
+
+def debug(string):
+	# print string
+	return
+
+# Python throws some errors here about 'key not defined'. It is defined in the data[i] = '=' block, which will be called (on properly formatted input) before 'key' is referenced.
+def parse(data, start=0):
+	data = data.strip()
+	debug(data)
+	out = {}
+	index = start
+	possible = start
+	i = start
+	recursed = True
+	while i < len(data):
+		debug(str(i)+' '+str(data[i]))
+		if data[i] == '[':
+			debug('Recursing...')
+			out[key], i = parse(data, i+1)
+			possible = start
+			recursed = True
+			debug('Recursion returned: '+str(out[key]))
+		elif data[i] == ']':
+			value = data[index:i]
+			debug('<1>Value: data['+str(index)+':'+str(i)+']='+str(value))
+			out[key] = value
+			return (out, i)
+		elif data[i] == ' ':
+			possible = i+1
+			debug('Possible key: data['+str(i+1)+':'+str(i+2)+']='+str(data[i+1:i+2]))
+		elif data[i] == '=':
+			if not recursed:
+				out[key] = data[index:possible-1]
+				debug('<2>Value: data['+str(index)+':'+str(possible-1)+']='+str(out[key]))
+			key = data[possible:i]
+			debug('Key: data['+str(possible)+':'+str(i)+']='+str(key))
+			index = i+1
+			recursed = False
+		i += 1
+	if not recursed: # The last k,v pair
+		out[key] = data[index:]
+		debug('<3>Value: data['+str(index)+':]='+str(out[key]))
+	return out
+
+# print '1', parse('')
+# print '2', parse('a=b')
+# print '3', parse('a=b c')
+# print '4', parse('a=[b=c]')
+# print '5', parse('a=b c=d')
+# print '6', parse('a=[b=c d]')
+# print '7', parse('a=[b=c] d=e')
+# print '8', parse('a=[b=c d=e]')
+# print '9', parse('a=b c d=e')
+# print '0', parse('a=b c=[d=e] f=g')
+# print 'A', parse('a=[b=c d=e] f=g')
+
+tail = Popen(['tail', '-f', config['log']+'Power.log'], stdout=PIPE)
+while True:
+	line = tail.stdout.readline()
+	line = line[19:]
+	# Initial hand
+	if line[:48] == 'GameState.DebugPrintEntityChoices() -   Entities':
+		data = parse(line[53:-2])
+		if data['player'] == '2':
+			print Hand.length(), data['id']
+			if Hand.length() == 4:
+				Hand.draw('The Coin')
+				Hand.wentFirst = 1
+				print Hand.hand[4].name
+			else:
+				Hand.draw(data['id'])
+	# Hand after mulligan
+	if line[:49] == 'GameState.DebugPrintEntitiesChosen() -   Entities':
+		data = parse(line[54:-2])
+		if data['player'] == '2':
+			Hand.mulligan(int(data['zonePos'])-1, data['id'])
+	if line[:48] == 'PowerTaskList.DebugPrintPower() -     TAG_CHANGE':
+		data = parse(line[48:])
+		# Drew a card
+		if data['tag'] == 'ZONE_POSITION':
+			if 'zone' in data['Entity'] and data['Entity']['zone'] == 'DECK':
+				if data['Entity']['player'] == '2':
+					Hand.draw(data['Entity']['id'])
+					print 'Drew a card', data['Entity']
+		# Played a card
+		elif data['tag'] == 'JUST_PLAYED':
+			if data['Entity']['zone'] == 'HAND':
+				if data['Entity']['player'] == '2':
+					Hand.play(int(data['Entity']['zonePos'])-1)
+					print '<143>Played', data['Entity']
+		# End of turn
+		elif data['tag'] == 'TURN':
+			Hand.turnover()
+		elif data['tag'] == 'STEP':
+			if data['value'] == 'FINAL_GAMEOVER':
+				Hand.reset()
+				print 'Game Over'
