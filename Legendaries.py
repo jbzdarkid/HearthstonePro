4245b5bfb27d21b1706da2028f4aea5bdbfb76a8
jbzdarkid <jbzdarkid@gmail.com>
2016-06-05 23:40:09 -0400

    small changes

--- Legendaries.py
+++ Legendaries.py
@@ -16,17 +16,41 @@ def blockEnd():
     varianWrynn = False
 
 def die(entity):
-    global nzoth
-    # Hard-code a list of deathrattle creatures
-    nzoth.append(entity['name'])
+    if entity['name'] in [
+        'Abomination', 'Anomalus', 'Anub\'ar Ambusher', 'Anub\'arak', 'Anubisath Sentinel',
+        'Bloodmage Thalnos', 'Boom Bot',
+        'Cairne Bloodhoof', 'Chillmaw', 'Clockwork Gnome', 'Corrupted Healbot',
+        'Dancing Swords', 'Dark Cultist', 'Darkshire Librarian', 'Darnassus Aspirant', 'Deathlord', 'Deathwing, Dragonlord', 'Dreadsteed',
+        'Explosive Sheep',
+        'Feugen', 'Fiery Bat',
+        'Harvest Golem', 'Haunted Creeper', 'Huge Toad',
+        'Infested Tauren', 'Infested Wolf',
+        'Leper Gnome', 'Loot Hoarder',
+        'Mad Scientist', 'Majordomo Executus', 'Malorne', 'Mechanical Yeti', 'Mounted Raptor',
+        'Nerubain Egg',
+        'Piloted Shredder', 'Piloted Sky Golem', 'Polluted Hoarder', 'Possessed Villager',
+        'Rhonin',
+        'Savannah Highmane', 'Selfless Hero', 'Shifting Shade', 'Sludge Belcher', 'Sneed\'s Old Shredder', 'Southsea Squidface', 'Spawn of N\'Zoth', 'Stalagg', 'Sylvannas Windrunner',
+        'Tentacle of N\'Zoth', 'The Beast', 'The Skeleton Knight', 'Tirion Fordring', 'Tomb Pillager', 'Toshley', 'Twilight Summoner',
+        'Undercity Huckster', 'Unstable Ghoul',
+        'Voidcaller',
+        'Webspinner', 'Wobbling Runts',
+        'Xaril, Poisoned Mind',
+        'Zealous Initiate', 'Zombie Chow'
+    ]:
+        global nzoth
+        if entity['player'] == Utilities.them:
+            nzoth[0].append(entity['name'])
+        elif entity['player'] == Utilities.us:
+            nzoth[1].append(entity['name'])
 
 def play2(entity):
     global cthun, yogg, reno, renoDuplicates
-    # Verify that this card was actually played by the opponent
+    # Verify that this card was actually from opp's deck, not generated
     if entity['name'] in reno:
-        renoDuplicates += 1
+        renoDuplicates.append(entity['name'])
     reno.append(entity['name'])
 
 def turnover():
     pass
-    # print 'Reno: %d duplicates, C'Thun: %d/%d, Yogg-Saron: %d spells\nN'Zoth: %s' % (renoDuplicates, cthun, cthun, yogg, ', '.join(nzoth))
+    # print 'Reno: %s, C'Thun: %d/%d, Yogg-Saron: %d spells\nN'Zoth: %s' % (', '.join(sorted(renoDuplicates)), cthun, cthun, yogg, ', '.join(sorted(nzoth)))
