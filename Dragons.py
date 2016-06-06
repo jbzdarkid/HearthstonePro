e8ec5dca987fd1d12dc9ac767175e6348705e9d1
jbzdarkid <jbzdarkid@gmail.com>
2016-06-06 19:19:14 -0400

    I'm unsure if any of this works.

--- Dragons.py
+++ Dragons.py
@@ -1,20 +1,26 @@
-# http://hearthstone.gamepedia.com/Enchantment_list#Regular_play
-
-# Todo: "Chillmaw"
-
 import Hand, Utilities
 
+# If a card is played and it *is* a dragon, remove any set it's a part of (no information )
+
 def reset():
-    global sets
+    global sets, noDragonBlock
     sets = []
+    noDragonBlock = False
 
 reset()
+
+def blockEnd():
+    if noDragonBlock:
+        noDragon()
+        noDragonBlock = False
 
 def play2(entity):
     if entity['player'] == Utilities.them:
         removeEntity(entity['id'])
         if entity['name'] in ["Blackwing Corruptor", "Rend Blackhand"]:
             noDragon()
+        elif entity['name'] in ["Alextrasza's Champion", "Wyrmrest Agent", "Blackwing Technician", "Twilight Guardian", "Twilight Whelp"]:
+            noDragonBlock = True
 
 def play3(entity, target):
     if entity['player'] == Utilities.them:
@@ -25,12 +31,14 @@ def play3(entity, target):
 # When a triggered ability enters play, usually attatched to another creature.
 def play4(entity):
     if entity['player'] == Utilities.them:
-        # "Alextrasza's Champion", "Wyrmrest Agent", "Blackwing Technician", "Twilight Guardian", "Twilight Whelp"
         if entity['name'] in ["Alexstrasza's Boon", "Bring it on!", "Dragon Blood", "Twilight's Embrace", "Twilight Endurance"]:
+            noDragonBlock = False
             hasDragon()
 
 def die(entity):
-    pass
+    if entity['player'] == Utilities.them:
+        if entity['name'] == "Chillmaw": # Needs to be able to fail
+            noDragonBlock = True
 
 def removeEntity(id):
     for i in range(len(sets)):
@@ -50,4 +58,7 @@ def noDragon():
         removeEntity(card.id)
 
 def turnover():
+    for set in sets:
+        if len(set) < 2:
+            
     pass
