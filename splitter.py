6563b29cd26677cbe6038169f1e0fc3c7570a10a
jbzdarkid <jbzdarkid@gmail.com>
2016-12-15 12:34:44 -0500

    Fixed up to work with new style, also cleaned up a bit

--- splitter.py
+++ splitter.py
@@ -7,7 +7,6 @@ if __name__ == '__main__': # pragma: no cover
     if rootDir:
         rootDir += sep
     if len(argv) == 1 or argv[1] == 'all':
-        from os import listdir
         files = listdir(rootDir+'tests')
     # elif argv[1] == 'latest':
     #   from os.path import getmtime
@@ -21,22 +20,17 @@ if __name__ == '__main__': # pragma: no cover
         buffer = ''
         cards = set()
         with open(rootDir+'tests'+sep+file, 'rb') as f:
-            data = f.read().split('\n')
-            for i in range(len(data)):
-                buffer += '\n'+data[i]
-                if 'PowerTaskList.DebugPrintPower() -     TAG_CHANGE Entity=GameEntity tag=STEP value=FINAL_GAMEOVER' in data[i]:
-                    if len(cards) == 0:
-                        break
+            for line in f:
+                buffer += line
+                if line[19:] == 'PowerTaskList.DebugPrintPower() -     TAG_CHANGE Entity=GameEntity tag=STEP value=FINAL_GAMEOVER\n':
                     name = ', '.join(sorted(cards))
                     with open(rootDir+'tests'+sep+name+'.log', 'wb') as g:
                         g.write(buffer)
                     buffer = ''
                     cards = set()
-                else:
-                    if 'ChoiceType=MULLIGAN' in data[i] and 'GameState.DebugPrintEntityChoices()' in data[i]:
-                        if config['username'] in data[i]:
-                            us = data[i][60]
-                    if 'Entity=[name=' in data[i] and 'player='+us not in data[i]:
-                        card = data[i].split('name=', 1)[1].split('=', 1)[0][:-3]
-                        if '"'+card+'"' in cardspy:
-                            cards.add(card)
+                elif 'ChoiceType=MULLIGAN' in line and config['username'] in line:
+                    us = line[60]
+                elif 'Entity=[name=' in line and 'player='+us not in line:
+                    card = line.split('=')[3][:-3]
+                    if '"'+card+'"' in cardspy:
+                        cards.add(card)
