4073bf8f21599cd2106ae30dca98aedf8aa07d8a
jbzdarkid <jbzdarkid@gmail.com>
2016-12-22 05:31:23 -0500

    Moved a comment, fixed an import

--- Dragons.py
+++ Dragons.py
@@ -1,10 +1,14 @@
+import logging
 import Hand, Utilities
 
-# So, each card needs a dragonSets and a notDragon field.
-# When a card is played and it fails to have a triggered effect, all cards in the hand set notDragon=True and dragonSets=[].
-# When a card is played and has a triggered effect, all cards in the hand with notDragon=False have dragonSets.append(newSet)
-# When a card is played and it's a dragon, for each set it's a part of, all cards in hand have dragonSets.remove(set). If the sets becomes empty, this DOES NOT mean the card isn't a dragon.
-# Each printout, any sets with 2 or 1 members should be mentioned.
+# There's a few things to do about dragons.
+# 1. When a card is played that has a triggered effect, >= 1 card in hand must be a dragon.
+# 1a. If (at some later date) a card is played from that set which is a dragon, no information is known about the remaining cards.
+# 1b. If (at some later date) all cards but one are played from that set and are not dragons, then the remaining card is a dragon.
+# 1c. Info from 1b. may be relevant with 2 cards remaining.
+# 2. When a card is played that fails to have a triggered effect, 0 cards in hand are dragons.
+# 2a. If (at some later date) a card is played which does have a triggered effect, these cards are excluded from the set.
+# 3. If a card which is not a dragon is returned to hand, we know that it still isn't a dragon.
 
 DRAGONS = [
     'Alextrasza', 'Azure Drake',
